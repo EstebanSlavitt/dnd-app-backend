43f2e0fd9872e0a15c4902e32ec0ba632085f982
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-08-27 16:36:47 -0700

    added CRUD

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -11,3 +11,22 @@ def create
   )
   render :show
 end
+
+def show
+  @character = Character.find_by(id: params[:id])
+  render :show
+end
+
+def update
+  @character = Character.find_by(id: params[:id])
+  @character.update(
+    name: params[:name] || @character.name,
+  )
+  render :show
+end
+
+def destroy
+  @character = Character.find_by(id: params[:id])
+  @character.destroy
+  render json: { message: "character destroyed successfully" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/characters" => "characters#index"
+  get "/characters/:id" => "characters#show"
   post "/characters" => "characters#create"
+  patch "/characters/:id" => "characters#index"
+  delete "/characters/:id" => "characters#destroy"
 end
