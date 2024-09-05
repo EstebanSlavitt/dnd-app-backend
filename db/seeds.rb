ca53668f044d6efc351b8c90fffc684b56074a36
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-09-05 09:30:10 -0700

    added dnd-api

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@ Character.create([
   { name: "Gandalf", class_name: "Wizard", race: "Maia", level: 20, background: "Sage", alignment: "Neutral Good" },
   { name: "Frodo", class_name: "Rogue", race: "Hobbit", level: 3, background: "Hermit", alignment: "Neutral Good" },
 ])
-# This is just some sample data i have
+
+require "rest-client"
+require "json"
+
+puts "Fetching classes from D&D 5e API..."
+classes_data = DndApiService.get_classes
+
+classes_data["results"].each do |class_data|
+  Character.create(
+    name: "Random Character",
+    class_name: class_data["name"],
+    race: "Human",  # You can later fetch races similarly
+    level: rand(1..20),
+    background: "Adventurer",
+    alignment: ["Lawful Good", "Neutral", "Chaotic Evil"].sample,
+  )
+end
+
+puts "Seeding complete!"
